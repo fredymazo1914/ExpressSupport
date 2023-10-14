2b97cd9107530616fe49cce1ecff50c9f19e2403
FREDY ANTONIO MAZO QUICENO <frema1914@gmail.com>
2023-10-14 13:58:03 -0500

    Created Class Entity.cs

---
+++ ExpressSupport/DAL/Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpressSupport.DAL.Entities
+{
+    public class Entity
+    {
+
+        public Guid Id { get; set; }//PK
+
+        public DateTime? CreateDate { get; set; }//? = campo Nulleable, es decir que se puden guardar nulls
+
+        public DateTime? ModifiedDate { get; set; }//? = campo Nulleable, es decir que se puden guardar nulls
+    }
+}
